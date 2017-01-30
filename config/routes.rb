dfc5c048a2b3dba0f93da0da76e3953dc75e3cc3
Muriel Berthou Crestey <murielberthoucrestey@gmail.com>
2017-01-30 20:52:15 +0100

    restart xoxo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  get 'pages/portraits'
+
+  get 'pages/bestiaries'
+
+  get 'pages/explorations'
+
+  get 'pages/tributes'
+
+  get 'pages/inspirations'
+
+  get 'pages/references'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/users/sign_in"
+
 end
